f22abda5450cb6f5387f6037c1fc215e05c16491
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-07 08:34:06 +0530

    new changes

--- lib/view/category_screen.dart
+++ lib/view/category_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:smachara_app/view/home_screen.dart';
+import 'package:smachara_app/view_model/news_view_model.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -8,8 +12,32 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  NewsViewModel newsViewModel = NewsViewModel();
+
+  final format = DateFormat('MMMM dd,yyyy');
+
+  String category = 'general';
+
+  List<String> categoryList = [
+    'General',
+    'Entertainment',
+    'Health',
+    'Sports',
+    'Business',
+    'Technology'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(),
+        ],
+      ),
+    );
   }
 }
