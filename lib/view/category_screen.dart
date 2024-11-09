1a386d30d067da7799ef39e9b80a11c6343d7db5
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2024-11-09 08:25:39 +0530

    new changes add

--- lib/view/category_screen.dart
+++ lib/view/category_screen.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
-import 'package:smachara_app/view/home_screen.dart';
+import 'package:smachara_app/models/news_category_model.dart';
 import 'package:smachara_app/view_model/news_view_model.dart';
 
 class CategoryScreen extends StatefulWidget {
@@ -74,9 +77,41 @@ class _CategoryScreenState extends State<CategoryScreen> {
                 }),
               ),
             ),
+            Expanded(
+              child: FutureBuilder<CategoriesNewsModel>(
+                future: newsViewModel.fetchNewsCategoriesApi(category),
+                builder: (BuildContext context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: SpinKitCircle(
+                        size: 50,
+                        color: Colors.orangeAccent,
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: snapshot.data!.articles!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        DateTime dataTime = DateTime.parse(
+                          snapshot.data!.articles![index].publishedAt
+                              .toString(),
+                        );
+                        return Container();
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+  final spinKit2 = const SpinKitFadingCircle(
+    color: Colors.amber,
+    size: 40,
+  );
 }
